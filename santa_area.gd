216081be98d6a1003c88399c9b3ce4c850d619cb
TobiMercader <Tobimercader@gmail.com>
2023-12-20 00:59:54 -0300

    santa mouse aim added

--- santa_area.gd
+++ santa_area.gd
@@ -1,5 +1,12 @@
 extends Area2D
 
+var aim_dir
+var santa_pos
+@export var aim_range : float
+
 func _process(delta):
-	print(self.position)
-	self.position = get_node("..").direction * 40
+	santa_pos = get_node("..").global_position 
+	aim_dir = (santa_pos - get_viewport().get_mouse_position()).normalized()
+	self.global_position = (santa_pos - aim_dir * aim_range)
+	print(santa_pos)
+	
