37bb2a8d5bc7e2d2a8e26be42c41dd1133f3a1b1
Alex <trevan98@gmail.com>
2024-01-28 05:34:45 +0300

    cleaning up, made attack animation

--- Enemies/BlueSlime/blue_slime.gd
+++ Enemies/BlueSlime/blue_slime.gd
@@ -3,7 +3,7 @@ extends CharacterBody2D
 
 @onready var player = get_node("/root/World/Player")
 
-func _physics_process(delta: float) -> void:
+func _physics_process(_delta: float) -> void:
 	velocity = position.direction_to(player.position) * 100
 	move_and_slide()
 	
--- Player/santa.gd
+++ Player/santa.gd
@@ -9,51 +9,60 @@ var bullet_path = preload("res://Player/Attacks/Default/default_attack.tscn")
 var is_flipped = false
 
 func movement(motion: Vector2) -> Vector2:
-  if Input.is_action_pressed("move_right"):
-    motion.x += 1
-  if Input.is_action_pressed("move_left"):
-    motion.x -= 1
-  if Input.is_action_pressed("move_down"):
-    motion.y += 1
-  if Input.is_action_pressed("move_up"):
-    motion.y -= 1
-  return motion
+	if Input.is_action_pressed("move_right"):
+		motion.x += 1
+	if Input.is_action_pressed("move_left"):
+		motion.x -= 1
+	if Input.is_action_pressed("move_down"):
+		motion.y += 1
+	if Input.is_action_pressed("move_up"):
+		motion.y -= 1
+
+	return motion
 
 func player_flip(cursor_position: Vector2) -> void:
-  if cursor_position.x < 0:
-      $AnimatedSprite2D.flip_h = true
-  else:
-      $AnimatedSprite2D.flip_h = false
+	if cursor_position.x < 0:
+			$AnimatedSprite2D.flip_h = true
+	else:
+			$AnimatedSprite2D.flip_h = false
 
-func default_attack() -> void:
-  if Input.is_action_just_pressed("attack"):
-    shoot()
+func play_attack_animation() -> void:
+	$AnimatedSprite2D.play("attack", 0.4)
 
-func shoot() -> void:
-  if timer.time_left > 0:
-    return
+func attack() -> void:
+	if timer.time_left > 0:
+		return
+		
+	$DefaultAttack.look_at(get_global_mouse_position())
+	var bullet := bullet_path.instantiate()
+	marker.add_child(bullet)
+	bullet.global_position = marker.global_position
+	bullet.global_rotation = marker.global_rotation
+	timer.start()
+	play_attack_animation()
 
-  $DefaultAttack.look_at(get_global_mouse_position())
-  $AnimatedSprite2D.play("attack")
-  var bullet := bullet_path.instantiate()
-  marker.add_child(bullet)
-  bullet.global_position = marker.global_position
-  bullet.global_rotation = marker.global_rotation
-  timer.start()
-  
+	
 func _physics_process(delta: float) -> void:
-  var motion = Vector2.ZERO	
-  motion = movement(motion)
-  var cursor_position := get_local_mouse_position()  
-  player_flip(cursor_position)
-  
-  if motion.length() > 0:
-    $AnimatedSprite2D.play("run")  
-    motion = motion.normalized() * speed
-  else:
-    $AnimatedSprite2D.play("idle")
-    
-  position += motion * delta
-  move_and_slide()
-  default_attack()
+	var motion = Vector2.ZERO	
+	motion = movement(motion).normalized() * speed
+	var cursor_position := get_local_mouse_position()  
+	player_flip(cursor_position)
+	
+	if motion.length() > 0:
+		if timer.time_left > 0:
+			play_attack_animation()
+		else:
+			$AnimatedSprite2D.play("run")  
+
+	else:
+		if timer.time_left > 0:
+			play_attack_animation()
+		else:
+			$AnimatedSprite2D.play("idle")
+		
+	position += motion * delta
+	move_and_slide()
+
+	if Input.is_action_just_pressed("attack"):
+		attack()
 	
