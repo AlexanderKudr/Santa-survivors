a64dba130a2ec49c490cee6ff090981587e786b6
Alex <trevan98@gmail.com>
2024-02-07 22:20:28 +0300

    wip blue slime attack, snowman movement

---
+++ Enemies/Snowman/mob_snowman.gd
@@ -0,0 +1,25 @@
+extends CharacterBody2D
+
+
+@onready var player = get_node("/root/World/Player")
+@export var speed = 20
+ 
+var is_flipped = false
+func _physics_process(delta: float) -> void:
+	velocity = position.direction_to(player.position) * speed
+	move_and_slide()
+	
+
+	if velocity.length() > 0:
+		$AnimationPlayer.play("run")  
+	
+		if velocity.x < 0:
+			$Sprite2D.flip_h = true
+			is_flipped = true
+		
+		else:
+			$Sprite2D.flip_h = false
+			is_flipped = false
+		
+	position += velocity * delta
+	move_and_slide()
